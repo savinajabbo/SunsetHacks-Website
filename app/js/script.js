1d250b6f78bbd01116d2932980fda9318b812a34
savina <savina.jabbo@gmail.com>
2023-11-20 16:50:11 -0800

    added overlay for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,24 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+    
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+
+});
+
+
 // FAQ ANIMATION
 document.querySelectorAll('.accordion__question').forEach((item) => {
     item.addEventListener('click', (event) => {
