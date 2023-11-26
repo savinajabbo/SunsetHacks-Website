459eb4ed1e7c4c89e28d108183f4b2d490539479
savina <savina.jabbo@gmail.com>
2023-11-25 21:11:25 -0800

    scroll added

--- app/js/script.js
+++ app/js/script.js
@@ -75,3 +75,12 @@ document.querySelectorAll('.accordion__question').forEach((item) => {
         item.classList.toggle('open');
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
